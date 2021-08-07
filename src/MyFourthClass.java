342593f4dbe449c2c4e91f1c027660fd7926495c
aturan10 <alituran1021@gmail.com>
2021-08-07 14:37:27 -0500

    commiting update

---
+++ src/MyFourthClass.java
@@ -0,0 +1,10 @@
+public class MyFourthClass {
+    public static void main(String[] args) {
+        int x=5;
+        int sum =x+2;
+        System.out.println(sum);
+        int y=0;
+        String str="Java";
+        System.out.println("This is rom");
+    }
+}
